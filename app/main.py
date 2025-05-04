5b5026f3f8a4cf8b973702e11bffc15af13534fb
Evandro Martinelli <evandro@evandro.net>
2025-05-04 15:26:25 -0300

    fix logs

--- app/main.py
+++ app/main.py
@@ -3,14 +3,10 @@ from mangum import Mangum
 from app.api.v1.endpoints import router as imoveis_router
 import logging
 
-if len(logging.getLogger().handlers) > 0:
-    # The Lambda environment pre-configures a handler logging to stderr. If a handler is already configured,
-    # `.basicConfig` does not execute. Thus we set the level directly.
+if logging.getLogger().hasHandlers():
     logging.getLogger().setLevel(logging.INFO)
 else:
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-logging.info("Lambda inicializada - teste de log")
 
 app = FastAPI(title="Scraper Imóveis Caixa")
 
